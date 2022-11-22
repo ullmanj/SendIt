e8d7e625e42c81dc845c04a225a8e6e179c52c36
Jamie Ullman <jamieMullman@gmail.com>
2022-11-22 16:37:04 -0500

    Interim Styled Tab Bar

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 // React Imports
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// Font and Icon Imports
+import { Octicons } from '@expo/vector-icons';
 // Local Imports
 import { HomeStack, SendStack, LogStack } from './components';
 
@@ -14,7 +16,26 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Navigator screenOptions={({ route }) => ({  // Adapted from cs47-lecture5b-demo
+          title: '',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'HomeStack') {
+              iconName = 'home'
+            } else if (route.name === 'SendStack') {
+              iconName = 'paper-airplane'
+            } else if (route.name === 'LogStack') {
+              iconName = 'clock'
+            }
+            // You can return any component that you like here!
+            return <Octicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        })} >
+
         <Tab.Screen name="HomeStack" component={HomeStack} />
         <Tab.Screen name="SendStack" component={SendStack} />
         <Tab.Screen name="LogStack" component={LogStack} />
