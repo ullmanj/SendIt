83a200b07e022b1b664d0b78d0ef07691f1afe4b
Jamie Ullman <jamieMullman@gmail.com>
2022-12-08 22:06:13 -0800

    Loading screen on timer

--- components/IntroScreen.js
+++ components/IntroScreen.js
@@ -6,20 +6,17 @@ import { TouchableOpacity } from 'react-native';
 export default function IntroScreen() {
     const navigation = useNavigation();
 
-    // WORKING CODE for loading on a timer
-    /*useEffect(() => {
-    const intervalId = setInterval(() => {
-        navigation.navigate('UpdateInterestsScreen');
-    }, 5000)
-    
-    return () => clearInterval(intervalId)
-    }, [])*/
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.navigate('UpdateInterestsScreen');
+        }, 3000)
+    }, [])
      
     return(
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <TouchableOpacity activeOpacity={1} onPress={() => navigation.navigate('UpdateInterestsScreen')}>
+            {/* <TouchableOpacity activeOpacity={1} onPress={() => navigation.navigate('UpdateInterestsScreen')}> */}
                 <Image style={{width: Dimensions.get('screen').width, height: Dimensions.get('screen').height}}source={require('../utils/miscPics/introScreen.png')}/>
-            </TouchableOpacity>
+            {/* </TouchableOpacity> */}
         </View>
     );
 }
